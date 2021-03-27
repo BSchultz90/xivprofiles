2dbcf64e34e7d7296852aacce06ed53254d3afbc
Brice Schultz <Briceschutz91@gmail.com>
2021-03-27 09:21:27 -0700

    Added Tailwind Dependancies / Tested  | Added Color Gradient

---
+++ components/Layout.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Layout = () => {
+  return (
+    <div className="bg-gradient-to-r from-gray-600 to-black  min-h-screen">
+      <h1>Test Layout</h1>
+    </div>
+  );
+};
+
+export default Layout;
